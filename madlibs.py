f7f6b0a1deb23d40305f57eb72438482ad20e212
aziz <azizalghanim98@gmail.com>
2022-04-06 01:29:30 +0300

    finished futoor madlib

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,32 @@
+
 def main():
-	# write your code here
+	# attempt to validate using function
+	'''	def string_validation(input):
+			while True:
+				try:
+					str(input)
+				except ValueError:
+					print('That doesn\t work!')
+				else:
+					break
+
+		def emotion():
+			value = str(input('Enter an Emotion: '))
+			return value
+
+		string_validation(emotion())'''
 
 
+	emotion = str(input('Enter an Emotion: '))
+	food = str(input('Enter your favorite food: '))
+	family = str(input('Enter a family members\' name: '))
+	time = int(input('Enter a number from 1 to 12: '))
+
+	print(f'\n I woke up feeling {emotion}, i went to the kitchen to eat some leftover {food}')
+	if time < 6:
+		print(f'when {family} looked at me with shock. I then relized that it was {time}, and I had eaten before futoor ')
+	else:
+		print(f'when I looked at the time and realized it was {time} and I had slept all day long')
 
 if __name__ == '__main__':
 	main()
