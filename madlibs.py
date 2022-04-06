33d310d0d440cc3d13d4a31bb81cc2a4c91d9dd1
aziz <azizalghanim98@gmail.com>
2022-04-07 00:13:34 +0300

    ta7seen

--- madlibs.py
+++ madlibs.py
@@ -20,13 +20,13 @@ def main():
 	emotion = str(input('Enter an Emotion: '))
 	food = str(input('Enter your favorite food: '))
 	family = str(input('Enter a family members\' name: '))
-	time = int(input('Enter a number from 1 to 12: '))
+	time = int(input('Enter a number from 0 to 24: '))
 
 	print(f'\n I woke up feeling {emotion}, i went to the kitchen to eat some leftover {food}')
-	if time < 6:
-		print(f'when {family} looked at me with shock. I then relized that it was {time}, and I had eaten before futoor ')
-	else:
-		print(f'when I looked at the time and realized it was {time} and I had slept all day long')
+	if time < 18 and time >= 4:
+		print(f'when {family} looked at me with shock. I then relized that it was {time} o\'clock, and I had eaten before futoor ')
+	elif time >= 18 or time < 4:
+		print(f'when I looked at the time and realized it was {time} o\'clock and I had slept all day long')
 
 if __name__ == '__main__':
 	main()
